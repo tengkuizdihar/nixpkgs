7db5ece23de368e7b5e96f4b299be09e8fcbb4d8
tengkuizdihar <tengkuizdihar@gmail.com>
2022-10-24 13:50:49 +0700

    scanmem gui add

--- pkgs/tools/misc/scanmem/default.nix
+++ pkgs/tools/misc/scanmem/default.nix
@@ -1,27 +1,31 @@
-{ lib, stdenv, autoconf, automake, intltool, libtool, fetchFromGitHub, readline }:
+{ lib, stdenv, autoconf, automake, intltool, libtool, fetchFromGitHub, readline, gameconqueror ? false }:
 
 stdenv.mkDerivation rec {
   version = "0.17";
   pname = "scanmem";
 
   src = fetchFromGitHub {
-    owner  = "scanmem";
-    repo   = "scanmem";
-    rev    = "v${version}";
+    owner = "scanmem";
+    repo = "scanmem";
+    rev = "v${version}";
     sha256 = "17p8sh0rj8yqz36ria5bp48c8523zzw3y9g8sbm2jwq7sc27i7s9";
   };
 
   nativeBuildInputs = [ autoconf automake intltool libtool ];
   buildInputs = [ readline ];
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
-  meta = with lib; {
-    homepage = "https://github.com/scanmem/scanmem";
-    description = "Memory scanner for finding and poking addresses in executing processes";
-    maintainers = [ ];
-    platforms = platforms.linux;
-    license = licenses.gpl3;
-  };
+  configurePhase =
+    lib.concatStringsSep "\n" ([
+      "./autogen.sh"
+      (optionalString gameconqueror "./configure --prefix=$out --enable-gui")
+    ]);
+
+  meta = with lib;
+    {
+      homepage = "https://github.com/scanmem/scanmem";
+      description = "Memory scanner for finding and poking addresses in executing processes";
+      maintainers = [ ];
+      platforms = platforms.linux;
+      license = licenses.gpl3;
+    };
 }
